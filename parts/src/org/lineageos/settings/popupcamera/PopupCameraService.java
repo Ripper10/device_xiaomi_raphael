2c924187085337a03301ca1fa1ad48cc55f476ef
Alex <badnwz.ad@gmail.com>
2020-03-20 00:35:32 +0200

    raphael: PopupService: Remove los dependencies

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -32,8 +32,7 @@ import android.os.IBinder;
 import android.os.UserHandle;
 import android.util.Log;
 
-import org.lineageos.settings.R;
-import org.lineageos.settings.utils.FileUtils;
+import com.android.internal.util.custom.FileUtils;
 
 import vendor.xiaomi.hardware.displayfeature.V1_0.IDisplayFeature;
 import vendor.xiaomi.hardware.motor.V1_0.IMotor;
@@ -73,8 +72,8 @@ public class PopupCameraService extends Service {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
-            if (lineageos.content.Intent.ACTION_CAMERA_STATUS_CHANGED.equals(action)) {
-                mCameraState = intent.getExtras().getString(lineageos.content.Intent.EXTRA_CAMERA_STATE);
+            if (android.content.Intent.ACTION_CAMERA_STATUS_CHANGED.equals(action)) {
+                mCameraState = intent.getExtras().getString(android.content.Intent.EXTRA_CAMERA_STATE);
                 updateMotor(mCameraState);
             }
         }
@@ -126,7 +125,7 @@ public class PopupCameraService extends Service {
     private void registerReceiver() {
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_SCREEN_OFF);
-        filter.addAction(lineageos.content.Intent.ACTION_CAMERA_STATUS_CHANGED);
+        filter.addAction(android.content.Intent.ACTION_CAMERA_STATUS_CHANGED);
         registerReceiver(mIntentReceiver, filter);
     }
 
