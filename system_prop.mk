4d03d15f1f2a9acab4edd563e14e440534c15d9e
Alex <badnwz.ad@gmail.com>
2020-04-08 22:54:10 +0300

    system prop: remove adb debug on boot

--- system_prop.mk
+++ system_prop.mk
@@ -166,13 +166,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
 
-# USB debugging
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp,adb \
-    ro.adb.secure=0 \
-    ro.secure=0 \
-    ro.debuggable=1
-
 # Build Official
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.havoc.maintainer=nuub1k
